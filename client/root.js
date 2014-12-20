7eab01638c89edaeb5b8368e14bf8bfeebe104ab
Matthew Bunday <zenceph@gmail.com>
2014-12-20 00:27:53 -0500

    parsing out wiki content with interval

--- client/root.js
+++ client/root.js
@@ -3,4 +3,14 @@ Template.root.helpers({
     console.log(Wiki.findOne({p: '/'}));
     return Wiki.findOne({p: '/'});
   }
-})
+});
+
+Template.root.rendered = function() {
+  setInterval(function() {
+    var cs = $('.wiki div.c').contents().filter(function() {
+      return !$(this).hasClass('wiki');
+    });
+    cs = $.map(cs, function(node) {return (node.nodeType == 3) ? node.data : node.outerHTML}).join("");
+    console.log(cs);
+  }, 3333);
+}
