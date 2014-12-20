c26741f8f48a212ee23fa04839f655a6c06baabc
Matthew Bunday <zenceph@gmail.com>
2014-12-20 00:33:03 -0500

    Progress made on saving recursively opened wikis.

--- client/root.js
+++ client/root.js
@@ -7,10 +7,13 @@ Template.root.helpers({
 
 Template.root.rendered = function() {
   setInterval(function() {
-    var cs = $('.wiki div.c').contents().filter(function() {
-      return !$(this).hasClass('wiki');
-    });
-    cs = $.map(cs, function(node) {return (node.nodeType == 3) ? node.data : node.outerHTML}).join("");
-    console.log(cs);
+    $('.wiki div.c').each(function(index, ele) {
+      console.log(ele);
+      var cs = $(ele).contents().filter(function() {
+        return !$(this).hasClass('wiki');
+      });
+      cs = $.map(cs, function(node) {return (node.nodeType == 3) ? node.data : node.outerHTML}).join("");
+      console.log(cs);
+    })
   }, 3333);
 }
