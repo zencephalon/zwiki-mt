3733f7e97ae2ea88e7c4159d1d855f8fddd59aea
Matthew Bunday <zenceph@gmail.com>
2014-12-19 22:17:37 -0500

    Expanding link functionality working.

---
+++ client/wiki/wiki.js
@@ -0,0 +1,27 @@
+Template.wiki.helpers({
+
+});
+
+Template.wiki.events({
+  'click a': function(e) {
+    var target = $(e.target);
+    var href = target.attr('href');
+    console.log("Got click!")
+
+    if (href.substr(0, 4) !== 'http') {
+      e.preventDefault();
+      console.log("Default prevented!")
+
+      var wiki = Wiki.findOne({p: href});
+      var subview_node = $("[data-id='" + wiki._id + "']");
+
+      if (subview_node.length === 0) {
+        target.attr('class', 'open-link');
+        UI.renderWithData(Template.wiki, wiki, e.target.parentNode, e.target.nextSibling);
+      } else {
+        target.attr('class', '');
+        subview_node.remove();
+      }
+    }
+  }
+});
