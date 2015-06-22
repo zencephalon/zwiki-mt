71c468fa533a767c01c94a68a8863698e745e466
Matthew Bunday <zenceph@gmail.com>
2015-06-22 17:35:18 -0400

    Add slugify method

--- model/wiki.js
+++ model/wiki.js
@@ -47,6 +47,16 @@ Wiki.prototype.update = function(update) {
   }
 }
 
+Wiki.slugify = function(text)
+{
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+}
+
 Wiki.subscriptions = function() {
   Meteor.publish("wikis", function() { return Wikis.find({}); });
 
