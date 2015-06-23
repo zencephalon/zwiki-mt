0f859979ef75fbdb0eb9a6b95f96a718f3bdaadf
Matthew Bunday <zenceph@gmail.com>
2015-06-23 11:24:02 -0400

    Add wiki new route and wiki redirect

--- router.js
+++ router.js
@@ -13,8 +13,32 @@ Router.route('/', {
   }
 });
 
+Router.route('/w/new', function() {
+  if (Meteor.userId()) {
+    var wiki = Wiki.create({uid: Meteor.userId()});
+
+    this.redirect('/w/' + wiki._id);
+  } else {
+    this.redirect('/');
+  }
+});
+
+Router.route('/w/:_id/:slug', function() {
+  this.wait(Meteor.subscribe('wiki', this.params._id));
+  if (this.ready()) {
+    var wiki = Wikis.findOne({_id: this.params._id});
+    this.render("wiki", {data: wiki});
+  } else {
+    this.render("loading");
+  }
+});
+
 Router.route('/w/:_id', function() {
-  this.render("wiki", {data: function() {
-    return Wiki.findOne({_id: this.params._id});
-  }})
-})
+  this.wait(Meteor.subscribe('wiki', this.params._id));
+  if (this.ready()) {
+    var wiki = Wikis.findOne({_id: this.params._id});
+    this.redirect('/w/' + this.params._id + '/' + wiki.slug);
+  } else {
+    this.render("loading");
+  }
+});
