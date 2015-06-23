febf954b2497111dbeeee14ca6a4db00d4444285
Matthew Bunday <zenceph@gmail.com>
2015-06-23 10:33:51 -0400

    Add makeLink method to Wiki

--- model/wiki.js
+++ model/wiki.js
@@ -56,14 +56,17 @@ Wiki.prototype.save = function(update) {
   this.update(update);
 }
 
-Wiki.slugify = function(text)
-{
+Wiki.slugify = function(text) {
   return text.toString().toLowerCase()
     .replace(/\s+/g, '-')           // Replace spaces with -
     .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
     .replace(/\-\-+/g, '-')         // Replace multiple - with single -
     .replace(/^-+/, '')             // Trim - from start of text
     .replace(/-+$/, '');            // Trim - from end of text
+}
+
+Wiki.makeLink = function(id) {
+  return "/w/" + id;
 }
 
 Wiki.subscriptions = function() {
