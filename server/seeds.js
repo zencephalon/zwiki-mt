cc170306e30328be73de453916d4ce6088c95916
Matthew Bunday <zenceph@gmail.com>
2015-06-23 10:34:27 -0400

    Use new link format in seed data

--- server/seeds.js
+++ server/seeds.js
@@ -7,7 +7,7 @@ function seedData() {
   var uid = Accounts.createUser({email: "mkbunday@gmail.com", password: "zen"});
   var luvism = Wiki.create({title: "Luvism", text: "I luv Luvism", uid: uid});
   var guiding_principles = Wiki.create({title: "Guiding Principles", text: "I follow four guiding principles", uid: uid});
-  var index = Wiki.create({title: "Zencephalon", text: "I follow <a href='" + luvism._id + "'>luvism</a> and my <a href='" + guiding_principles._id + "'>guiding principles</a>.", root: true, uid: uid});
+  var index = Wiki.create({title: "Zencephalon", text: "I follow <a href='" + Wiki.makeLink(luvism._id) + "'>luvism</a> and my <a href='" + Wiki.makeLink(guiding_principles._id) + "'>guiding principles</a>.", root: true, uid: uid});
 }
 
 if (Meteor.isServer) {
