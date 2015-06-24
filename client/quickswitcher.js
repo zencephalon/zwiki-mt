c9f99147e83f31a696984ca5c9d2784d98fcde78
Matthew Bunday <zenceph@gmail.com>
2015-06-24 15:56:34 -0400

    Import quickswitcher from zencephalon-mt

---
+++ client/quickswitcher.js
@@ -0,0 +1,46 @@
+Template.quickswitcher.helpers({
+  settings: function() {
+    return {
+      position: "bottom",
+      limit: 5,
+      rules: [{
+        token: ':',
+        replacement: '',
+        end_token: '',
+        collection: Wikis,
+        field: "slug",
+        template: Template.link_preview,
+        callback: function(doc, element) {
+
+        }
+      }]
+    }
+  }
+});
+
+Template.quickswitcher.events({    
+  'submit form': function(event) {     
+    event.preventDefault();    
+  }    
+});
+
+Template.quickswitcher.rendered = function() {
+  Mousetrap.bind('ctrl+space', function(e) { 
+    e.preventDefault();
+    $('#quickswitcher').show();
+
+    // Save the viewport
+    // View.save(e.target);
+
+    $('#switcher').focus().val(":");
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    return false;
+  });
+  $(document).ready(function() {
+    $('#quickswitcher').focusout(function() {
+      $('#quickswitcher').hide();
+      // Restore the viewport
+      // View.restore_delayed();
+    })
+  });
+}
