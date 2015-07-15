182373b7b65579d2b5dd156a8b82b5a5bb0e558a
Matthew Bunday <zenceph@gmail.com>
2015-07-14 23:44:55 -0400

    Move saveFunction code into WikiView

--- client/lib/wiki_view.js
+++ client/lib/wiki_view.js
@@ -13,5 +13,28 @@ WikiView = {
     if (focusedId) {
       return $("div.wiki[data-id=" + this.focusedId() + "]");
     }
+  },
+  saveFunction: function() {
+    var $focusedWiki = WikiView.focusedWikiElement();
+    if ($focusedWiki) {
+      var $ele = $focusedWiki;
+      var $content = $ele.children('.content');
+      var $title = $ele.children('.title');
+
+      var content = $content.html();
+
+      var child_content = $.map($content.children('.wiki'), function(ele) {return ele.outerHTML});
+
+      child_content.forEach(function(c) {
+        content = content.replace(c, "");
+      })
+
+      content = content.replace(/\ open-link="true"/g, "");
+      content = content.replace(/\ open-link/g, "");
+
+      var t = $title.text();
+      var wiki = Wiki.findOne($ele.data('id'));
+      wiki.save({title: t, text: content});
+    }
   }
 }
--- client/wiki/wiki.js
+++ client/wiki/wiki.js
@@ -48,30 +48,7 @@ Template.wiki.rendered = function() {
     }
   });
 
-  var saveFunction = function() {
-    var $focusedWiki = WikiView.focusedWikiElement();
-    if ($focusedWiki) {
-      var $ele = $focusedWiki;
-      var $content = $ele.children('.content');
-      var $title = $ele.children('.title');
-
-      var content = $content.html();
-
-      var child_content = $.map($content.children('.wiki'), function(ele) {return ele.outerHTML});
-
-      child_content.forEach(function(c) {
-        content = content.replace(c, "");
-      })
-
-      content = content.replace(/\ open-link="true"/g, "");
-      content = content.replace(/\ open-link/g, "");
-
-      var t = $title.text();
-      var wiki = Wiki.findOne($ele.data('id'));
-      wiki.save({title: t, text: content});
-    }
-  };
-  saveFunction = _.debounce(saveFunction, 300);
+  var saveFunction = _.debounce(WikiView.saveFunction, 300);
 
   $('.wiki').keypress(saveFunction);
 }
