e4d680aa3b2ac50f7b6d7b33504bc4b9618d933f
Matthew Bunday <zenceph@gmail.com>
2015-07-16 10:44:39 -0400

    Refactor link click event

--- client/layout.js
+++ client/layout.js
@@ -17,30 +17,12 @@ Template.layout.rendered = function() {
 
 Template.layout.events({
   'click a': function(e) {
-    var target = $(e.target);
-    var href = target.attr('href');
+    e.preventDefault();
+    var link_id = $(e.target).data('id')
 
-    if (href = href.match(/^\/w\/(.*)/)) {
-      href = href[1];
-
-      e.preventDefault();
-
-      var wiki = Wiki.findOne({_id: href});
-      var selector = "[data-id='" + wiki._id + "']";
-      var subview_node = target.parent().children(selector);
-      var wiki_node = $(selector);
-
-      if (subview_node.length === 0) {
-        if (wiki_node.length === 0) {
-          target.attr('open-link', 'true');
-          UI.renderWithData(Template.wiki, wiki, e.target.parentNode, e.target.nextSibling);
-        } else {
-          wiki_node.focus();
-        }
-      } else {
-        target.attr('open-link', '');
-        subview_node.remove();
-      }
+    if (link_id) {
+      WikiView.focusLink(link_id);
+      WikiView.openFocusedLink();
     }
   }
 })
