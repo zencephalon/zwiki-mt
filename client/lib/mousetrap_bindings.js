308e57c56020a39df30dfdcaa68fb9d4efe30324
Matthew Bunday <zenceph@gmail.com>
2015-07-20 18:35:37 -0400

    Add key binding to select content area of a wiki

--- client/lib/mousetrap_bindings.js
+++ client/lib/mousetrap_bindings.js
@@ -7,7 +7,7 @@ Mousetrap.bind('alt+right', function(e) {
 Mousetrap.bind('alt+left', function(e) {
   e.preventDefault();
   WikiView.focusPrevLink();
-})
+});
 
 Mousetrap.bind('alt+down', function(e) {
   e.preventDefault();
@@ -19,7 +19,16 @@ Mousetrap.bind('alt+down', function(e) {
 Mousetrap.bind('alt+up', function(e) {
   e.preventDefault();
   WikiView.focusParentWiki();
-})
+});
+
+Mousetrap.bind('tab', function(e) {
+  e.preventDefault();
+  var $child_content = WikiView.focusedWikiElement().children('.content');
+  $child_content.focus();
+  var range = rangy.createRange();
+  range.setStart($child_content[0]);
+  rangy.getSelection().setSingleRange(range);
+});
 
 // Open link
 Mousetrap.bind('alt+space', function(e) {
