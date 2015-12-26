40b9856285343822da7ebce356197213f7ca900f
Matthew Bunday <zenceph@gmail.com>
2015-12-26 00:54:58 -0600

    Clicking on a wiki moves the focus to the wiki

--- client/wiki/wiki.js
+++ client/wiki/wiki.js
@@ -13,25 +13,36 @@ Template.wiki.helpers({
 Template.wiki.rendered = function() {
   var self = Template.instance();
   var oldWikiId = undefined;
+  var id = Template.currentData()._id;
 
   if (! WikiView.focusedId()) {
-    WikiView.focus(Template.currentData()._id);
+    WikiView.focus(id);
   }
 
   this.autorun(function() {
-    if (Template.currentData()._id != oldWikiId) {
-      var wiki = Wiki.findOne({_id: Template.currentData()._id});
+    id = Template.currentData()._id;
+    if (id != oldWikiId) {
+      var wiki = Wiki.findOne({_id: id});
       wiki.subscribeToLinked();
 
       self.$('div.content[data-id=' + wiki._id + ']').html(wiki.text);
       self.$('h2[data-id=' + wiki._id + ']').html(wiki.title);
-      oldWikiId = Template.currentData()._id;
+      oldWikiId = id;
     }
   })
 
+  self.$('.wiki').click(function(e) {
+    var $target = $(e.target);
+    if ($target.is('a')) { return; }
+    var dataId = $target.data('id');
+    if (dataId) {
+      WikiView.focus(dataId);
+    }
+  });
+
   var saveDebounced = _.debounce(WikiView.save, 300);
 
-  $('.wiki').keypress(saveDebounced);
+  self.$('.wiki').keypress(saveDebounced);
 }
 
 Template.wiki.events({
